5d8b39d399e36428ab467c0608670efb73f98705
elaintran <elain.ttran@gmail.com>
2019-07-03 12:47:08 -0500

    moved routing information to htmlRoutes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var port = 3000;
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+})
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+})
+
+app.listen(3000 || PROCESS.ENV.PORT, function() {
+    console.log("Listening on port " + port);
+})
--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var express = require("express");
-var path = require("path");
 var app = express();
 var port = 3000;
-
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-})
-
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-})
 
 app.listen(3000 || PROCESS.ENV.PORT, function() {
     console.log("Listening on port " + port);
