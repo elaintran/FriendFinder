2311b0d6b786ac567f3cae31d9e673ebeaf62f4d
elaintran <elain.ttran@gmail.com>
2019-07-03 21:25:53 -0500

    route testing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,4 @@
-var express = require("express");
 var path = require("path");
-var app = express();
-var port = 3000 || process.env.PORT;
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
@@ -9,8 +6,4 @@ app.get("/", function(req, res) {
 
 app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
-})
-
-app.listen(port, function() {
-    console.log("Listening on port " + port);
 })
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require("express");
+var path = require("path");
 var app = express();
 var port = 3000;
 
-app.listen(3000 || PROCESS.ENV.PORT, function() {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+})
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+})
+
+//make public folder available to use
+app.use(express.static('app/public'));
+
+//
+app.listen(3000 || process.env.PORT, function() {
     console.log("Listening on port " + port);
 })
