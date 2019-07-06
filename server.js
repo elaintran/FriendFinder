dfefcee47124272c20f651260d6ca957f20e74bc
elaintran <elain.ttran@gmail.com>
2019-07-05 21:42:42 -0500

    get form information from index and store in json form

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
 var express = require("express");
-var path = require("path");
 var app = express();
+var path = require("path");
 var port = 3000 || process.env.PORT;
+
+var userForm = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//make public folder available to use
+app.use(express.static('app/public'));
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "app/public/home.html"));
 })
 
-app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+app.get("/api/user", function(req, res) {
+    return res.json(userForm);
 })
 
-//make public folder available to use
-app.use(express.static('app/public'));
+app.post("/api/user", function(req, res) {
+    var newUser = req.body;
+    userForm.push(newUser);
+    res.json(newUser);
+})
+
+app.post("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+})
 
 //set up port for Heroku
 app.listen(port, function() {
